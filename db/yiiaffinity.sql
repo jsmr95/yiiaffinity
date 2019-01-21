5b173644671501bacb0e8828cf467b201ac37a14
Jose Maria Gallego <josemaria.gallego@iesdonana.org>
2019-01-21 18:35:49 +0100

    Añado la tabla participaciones

--- db/yiiaffinity.sql
+++ db/yiiaffinity.sql
@@ -50,6 +50,33 @@ CREATE TABLE papeles
   , papel    VARCHAR(150) NOT NULL UNIQUE
 );
 
+DROP TABLE IF EXISTS papeles CASCADE;
+
+CREATE TABLE papeles
+(
+    id       BIGSERIAL   PRIMARY KEY
+  , papel    VARCHAR(150) NOT NULL UNIQUE
+);
+
+DROP TABLE IF EXISTS participaciones CASCADE;
+
+CREATE TABLE participaciones
+(
+    pelicula_id BIGINT       NOT NULL
+                             REFERENCES peliculas (id)
+                             ON DELETE NO ACTION
+                             ON UPDATE CASCADE
+    , persona_id BIGINT       NOT NULL
+                             REFERENCES personas (id)
+                             ON DELETE NO ACTION
+                             ON UPDATE CASCADE
+    , papel_id BIGINT       NOT NULL
+                             REFERENCES papeles (id)
+                             ON DELETE NO ACTION
+                             ON UPDATE CASCADE
+    , PRIMARY KEY (pelicula_id,persona_id,papel_id)
+);
+
 -- INSERT
 
 INSERT INTO usuarios (login, password)
@@ -79,6 +106,11 @@ VALUES ('Steven Yellow')
 
 INSERT INTO papeles (papel)
 VALUES ('Director')
-    , ('Actor/Actriz')
+    , ('Protagonista')
+    , ('Secundario')
     , ('Guionista')
     , ('Actor de doblaje');
+
+INSERT INTO participaciones (pelicula_id, persona_id, papel_id)
+VALUE (1,1,1) , (1,2,2) , (2,3,4) , (3,4,5) , (3,5,3) , (2,6,2) , (1,7,3)
+    , (2,2,1) , (1,7,3) 
