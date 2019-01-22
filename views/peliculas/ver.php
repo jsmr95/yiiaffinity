a387d77d1f256b03bed75f3bc72793ca21d446d8
Jose Maria Gallego <josemaria.gallego@iesdonana.org>
2019-01-22 20:08:48 +0100

    Modifico el ver de peliculas

--- views/peliculas/ver.php
+++ views/peliculas/ver.php
@@ -13,7 +13,7 @@ $inputOptions = [
 ?>
 
 <?php foreach ($pelicula->participaciones as $participacion): ?>
-    <dl>
+    <dl class="dl-horizontal">
         <dt>Nombre</dt>
         <dd><?= $participacion->persona->nombre ?></dd>
         <dt>Papel</dt>
